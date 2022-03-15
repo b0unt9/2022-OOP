2a00f1b9c2a1b842de6be39d9ff8cb2c8cb257f1
KSH <40918958+gitksh@users.noreply.github.com>
2022-03-15 21:00:00 +0900

    2 Weak

---
+++ 2w/mission/mission/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                int year = int.Parse(Console.ReadLine());
+                if (year < 1 || year > 4000) return;
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) Console.WriteLine("윤년");
+                else Console.WriteLine("평년");
+            }
+           
+        }
+    }
+}
