b7fddccaefb88e2cd274ce353065a06185baeebc
KSH <40918958+gitksh@users.noreply.github.com>
2022-03-22 21:00:00 +0900

    3 Weak

---
+++ 3w/mission/mission/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i++ < 10)
+            {
+                if (i % 2 == 0) continue;
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
