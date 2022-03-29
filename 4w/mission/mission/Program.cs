210cf482b5e52b369fbc9bc7663d4f17aaadd341
KSH <40918958+gitksh@users.noreply.github.com>
2022-03-29 21:00:00 +0900

    4 Weak

---
+++ 4w/mission/mission/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[5];
+
+        for (int i = 0; i < array.Length; ++i)
+        {
+            Console.WriteLine($"{i}번째 번호");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        Calculate(array);
+    }
+
+    static void Calculate(int [] array)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < array.Length; i++) sum += array[i];
+
+        Console.WriteLine($"합: {sum}");
+        Console.WriteLine($"평균: {(double) sum / array.Length}");
+    }
+}
